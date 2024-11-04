ac85748e5a1b886239f6baf0b0da43d3edbbfcce
Fahrifu <82388717+Fahrifu@users.noreply.github.com>
2024-11-04 12:42:37 +0100

    Updated Game to fit changes

--- Projects/Battleship/battleships-main/game.mjs
+++ Projects/Battleship/battleships-main/game.mjs
@@ -1,104 +1,95 @@
 import { ANSI } from "./utils/ansi.mjs";
-import { print, clearScreen } from "./utils/io.mjs";
+import { print, clearScreen, printCentered } from "./utils/io.mjs";
 import SplashScreen from "./game/splash.mjs";
-import { FIRST_PLAYER, SECOND_PLAYER } from "./consts.mjs";
 import createMenu from "./utils/menu.mjs";
 import createMapLayoutScreen from "./game/mapLayoutScreen.mjs";
 import createInBetweenScreen from "./game/inBetweenScreen.mjs";
 import createBattleshipScreen from "./game/battleshipsScreen.mjs";
 import { setLanguage, t } from "./utils/dictionary.mjs";
+import { FIRST_PLAYER, SECOND_PLAYER, GAME_BOARD_DIM } from "./consts.mjs";
 
-
-const GAME_FPS = 1000 / 60; 
 let currentState = null;    
 let gameLoop = null;        
-let mainMenuScene = null;
 
-function languageSelectionMenu() {
-    let menuItemCount = 0;
-    return createMenu([
-        {
-            text: "English", id: menuItemCount++, action: function () {
-                setLanguage("en");
-                initializeMainMenu();
-            }
-        },
-        {
-        text: "Norsk", id: menuItemCount++, action: function () {
-            setLanguage("no");
-            initializeMainMenu();
-            }
-        }
-    ]);
-}
-
-function initializeMainMenu() {
-    mainMenuScene = createMenu(buildMainMenu());
-    SplashScreen.next = mainMenuScene;
-    currentState = SplashScreen;
-    gameLoop = setInterval(update, GAME_FPS);
-}
-
-
-
-function buildMainMenu() {
-    let menuItemCount = 0;
-    return [
-        {
-            text: t("start_game"), id: menuItemCount++, action: function () {
-                clearScreen();
-                let inBetween = createInBetweenScreen();
-                inBetween.init(`${t("SHIP_PLACEMENT")}\n${t("First_player")}\n${t("player_look_away")}`, () => {
-
-                    let p1map = createMapLayoutScreen();
-                    p1map.init(FIRST_PLAYER, (player1ShipMap) => {
-
-
-                        let inBetween = createInBetweenScreen();
-                        inBetween.init(`${t("SHIP_PLACEMENT")}\n${t("First_player")}\n${t("player_look_away")}`, () => {
-                            let p2map = createMapLayoutScreen();
-                            p2map.init(SECOND_PLAYER, (player2ShipMap) => {
-                                return createBattleshipScreen(player1ShipMap, player2ShipMap);
-                            })
-                            return p2map;
-                        });
-                        return inBetween;
-                    });
-
-                    return p1map;
-
-                }, 3);
-                currentState.next = inBetween;
-                currentState.transitionTo = "Map layout";
-            }
-        },
-        { text: t("Exit Game"), id: menuItemCount++, action: function () { print(ANSI.SHOW_CURSOR); clearScreen(); process.exit(); } },
-    ];
-}
-
-(function initialize() {
-    print(ANSI.HIDE_CURSOR);
-    clearScreen();
-
-    if (!checkResolution()) {
-        console.log(t("resize_prompt"));
-        process.exit(1);
-    }
-
-    currentState = languageSelectionMenu();
-    gameLoop = setInterval(update, GAME_FPS); // The game is started.
-})();
-
-function update() {
-    currentState.update(GAME_FPS);
-    currentState.draw(GAME_FPS);
-    if (currentState.transitionTo != null) {
-        currentState = currentState.next;
-        print(ANSI.CLEAR_SCREEN, ANSI.CURSOR_HOME);
-    }
-}
+const REQUIRED_COLUMNS = 80;
+const REQUIRED_ROWS = 24;
 
 function checkResolution() {
     const { columns, rows } = process.stdout;
-    return columns >= 80 && rows >= 24; 
+    if (columns < REQUIRED_COLUMNS || rows < REQUIRED_ROWS) {
+        clearScreen();
+        printCentered(`Minimum required: ${REQUIRED_COLUMNS}x${REQUIRED_ROWS}\n`);
+        printCentered(`Current size: ${columns}x${rows}\n`);
+        printCentered(`Please resize your terminal and press Enter to continue.`);
+
+        process.stdin.once("keypress", () => {
+            checkResolution();
+        });
+        return false;
+    }
+    return true;
 }
+
+function initializeMainMenu() {
+    const mainMenuScene = createMenu([
+        { text: t("start_game"), action: transitionToMapLayout },
+        { text: t("select_board_size"), action: () => transitionTo(selectBoardSizeMenu())},
+        { text: t("exit_game"), action: () => process.exit() }
+    ]);
+
+    SplashScreen.next = mainMenuScene;
+    currentState = SplashScreen;
+    gameLoop = setInterval(update, 1000 / 60);
+}
+
+function selectBoardSizeMenu() {
+    return createMenu([
+        { text: t("board_small"), action: () => setBoardSize(8) },
+        { text: t("board_medium"), action: () => setBoardSize(10) },
+        { text: t("board_large"), action: () => setBoardSize(12) }
+    ]);
+}
+
+function setBoardSize(size) {
+    setBoardSize(size);
+    initializeMainMenu();
+}
+
+function transitionToMapLayout() {
+    const mapLayoutScreen = createMapLayoutScreen();
+    mapLayoutScreen.init(FIRST_PLAYER, () => createInBetweenScreen(
+        t("next_player_prompt", { player: "Player 2" }), () => {
+            const player2Layout = createMapLayoutScreen();
+            player2Layout.init(SECOND_PLAYER, () => createBattleshipScreen());
+            return player2Layout;
+        }
+    ));
+    currentState = mapLayoutScreen;
+}
+
+function transitionTo(newState) {
+    currentState = newState
+}
+
+function update(dt) {
+    currentState.update(dt);
+    currentState.draw(dt);
+
+    if (currentState.transitionTo) {
+        currentState = currentState.next;
+        clearScreen();
+    }
+}
+
+function initialize() {
+    print(ANSI.HIDE_CURSOR);
+    clearScreen();
+
+    if(!checkResolution()) {
+        return;
+    }
+    
+    initializeMainMenu();
+}
+
+initialize();
